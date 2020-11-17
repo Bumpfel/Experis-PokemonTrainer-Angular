e8888450644cbbf21a5bdd64f48d6549320441d3
Bumpfel <eric.enoksson@gmail.com>
2020-11-17 16:55:36 +0100

    chore(models): pokemon, trainer

--- src/app/interfaces/pokemon.ts
+++ src/app/interfaces/pokemon.ts
@@ -1,4 +1,12 @@
 export interface Pokemon {
     name: string
     imgUrl: string
+
+    types?: string[]
+    baseStat?: Map<string, number>
+    height?: number
+    weight?: number
+    abilities?: string[]
+    baseXP?: number
+    moves?: string[]
 }
