5eccbeea3eace91308ef950c536b7eb27a44b528
PerChrister <perchrister96@gmail.com>
2020-11-18 13:44:02 +0100

    fix: Pokemon Card Style Showcase

--- src/app/pages/pokemoncatalogue/pokemoncatalogue.component.ts
+++ src/app/pages/pokemoncatalogue/pokemoncatalogue.component.ts
@@ -9,32 +9,19 @@ import { FetchService } from 'src/app/services/fetch.service';
 })
 export class PokemonCatalogueComponent implements OnInit {
 
-  pokemon: Pokemon = { 
-    name: 'Pikachu', 
-    imgUrl: 'https://vignette.wikia.nocookie.net/sonicpokemon/images/7/77/Pikachu.png/revision/latest/scale-to-width-down/1000?cb=20200831092023' 
-  }
-  pokemon2: Pokemon = { 
-    name: 'Squirtle', 
-    imgUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png' 
-  }
-  pokemon3: Pokemon = { 
-    name: 'Bulbasaur', 
-    imgUrl: 'https://cdn.bulbagarden.net/upload/2/21/001Bulbasaur.png' 
-  }
-  pokemon4: Pokemon = { 
-    name: 'Charmander', 
-    imgUrl: 'https://vignette.wikia.nocookie.net/sonicpokemon/images/e/e0/Charmander_AG_anime.png/revision/latest?cb=20130714191911' 
-  }
-  pokemon5: Pokemon = { 
-    name: 'Meowth', 
-    imgUrl: 'https://vignette.wikia.nocookie.net/sonicpokemon/images/0/02/Meowth_anime.png/revision/latest?cb=20130624045447' 
-  }
-
-  pokemons: any = [this.pokemon, this.pokemon2, this.pokemon3, this.pokemon4, this.pokemon5];
-
-  constructor() { }
+  pokemonz: Pokemon[] | undefined;
+  
+  constructor(private fetchService: FetchService) {
+    console.log(this.fetchService);
+   }
 
   ngOnInit(): void {
+  }
+
+  getPokemons() {
+  this.fetchService.getPokemons().then(data => {
+    this.pokemonz = data;
+  });
   }
 
 }
