52aef4d9f57b6a1bd44972bebb4df8c52b4ff074
Bumpfel <eric.enoksson@gmail.com>
2020-11-19 10:59:34 +0100

    feat(auth/storage service): complete basic login functionality

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -11,16 +11,27 @@ export class AuthService {
   constructor(private storageService: StorageService) { }
 
   login(name: string): boolean {
-    if(name.trim().length === 0) {
-      return false
+    if(this.validateName(name)) {
+      this.storageService.save({ name })
+      return true
     }
-    
-    const trainer: Trainer = { name }
-    this.storageService.save(trainer)
-    return true
+    return false
+  }
+
+  logout() {
+    // destroys current trainer
+    this.storageService.resetActiveTrainer()
   }
 
   isLoggedIn(): boolean {
     return this.storageService.load() != null
+  }
+
+  getLoggedInTrainer(): Trainer | undefined {
+    return this.storageService.load()
+  }
+
+  private validateName(name: string): boolean {
+    return name.trim().length !== 0
   }
 }
